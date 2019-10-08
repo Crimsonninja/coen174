f93b3051a47b8aa542b3017126cfaebdbb20f5b3
jtrinh <johnnytrinh2010@gmail.com>
2019-10-07 21:37:35 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    #name = request.args.get("name", "World")
+    #return f'Hello, {escape(name)}!'
+    return "Hello World!"
