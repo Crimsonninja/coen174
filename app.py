0ee0f869a373450a1d54f8a5acbb4dc8bc642dea
jtrinh <johnnytrinh2010@gmail.com>
2019-11-02 12:51:03 -0700

    Add Google Sign-in

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-import os
+import os, json
 from flask import Flask, escape, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import *
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+# from flask_security import Security, SQLAlchemyUserDatastore, \
+#    login_required, current_user, login_user, logout_user
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Creating database
 db = SQLAlchemy(app)
+
+# Google Login Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 # Initializing Bootstrap
 Bootstrap(app)
@@ -24,7 +54,7 @@ topbar = Navbar('',
     View('Activities','activities'),
     View('Leaderboard','leaderboard'),
     View('Teams','teams'),
-    Link('Logout', 'http://jonathantrinh.com')
+    View('Logout','logout')
     # View('Your Account', 'frontend.account_info'),
 )
 
@@ -32,13 +62,118 @@ nav.register_element('top', topbar)
 
 nav.init_app(app)
 
-from models import Team, User, Activity
+from models import Team, User, Activity, User, Role
 
+# user_datastore = SQLAlchemyUserDatastore(db.session, User, Role)
+# security = Security(app, user_datastore)
+
+# LOGIN Section
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+def get_google_provider_cfg():
+  return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+  # Get authorization code Google sent back to you
+  code = request.args.get("code")
+
+  # Find out what URL to hit to get tokens that allow you to ask for
+  # things on behalf of a user
+  google_provider_cfg = get_google_provider_cfg()
+  token_endpoint = google_provider_cfg["token_endpoint"]
+
+  # Prepare and send a request to get tokens! Yay tokens!
+  token_url, headers, body = client.prepare_token_request(
+      token_endpoint,
+      authorization_response=request.url,
+      redirect_url=request.base_url,
+      code=code
+  )
+  token_response = requests.post(
+      token_url,
+      headers=headers,
+      data=body,
+      auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+  )
+
+  # Parse the tokens!
+  client.parse_request_body_response(json.dumps(token_response.json()))
+
+  # Now that you have tokens (yay) let's find and hit the URL
+  # from Google that gives you the user's profile information,
+  # including their Google profile image and email
+  userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+  uri, headers, body = client.add_token(userinfo_endpoint)
+  userinfo_response = requests.get(uri, headers=headers, data=body)
+
+  # You want to make sure their email is verified.
+  # The user authenticated with Google, authorized your
+  # app, and now you've verified their email through Google!
+  if userinfo_response.json().get("email_verified"):
+      #unique_id = userinfo_response.json()["sub"]
+      users_email = userinfo_response.json()["email"]
+      users_first_name = userinfo_response.json()["given_name"]
+      users_last_name = userinfo_response.json()["family_name"]
+  else:
+      return "User email not available or not verified by Google.", 400
+
+  print(f'USERS FIRST NAME IS {users_first_name}!')
+  # Create a user in your db with the information provided
+  # by Google
+
+
+  # print(user.first_name)
+  # print("The Filter")
+  # print(User.query.filter(User.email==user.email))
+
+  # Doesn't exist? Add it to the database.
+  if not User.query.filter(User.email==users_email).all():
+    user = User(first_name=users_first_name, last_name=users_last_name, email=users_email, active=True)
+    db.session.add(user)
+    db.session.commit()
+    print("COMMITED")
+  else:
+    user = User.query.filter(User.email==users_email).first()
+    #User.create(unique_id, users_name, users_email, picture)
+
+  # Begin user session by logging the user in
+  login_user(user)
+
+  # Send user back to homepage
+  return redirect(url_for("index"))
+
+@app.route("/logout")
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("index"))
 
 @app.route('/')
 def index():
-  print(os.getenv('APP_SETTINGS'))
-  return render_template('index.html', user_first_name=User.query.first().first_name)
+  if current_user.is_authenticated:
+    return render_template('index.html', user_first_name=current_user.first_name)
+  else:
+    # return '<a class="button" href="/login">Google Login</a>'
+    return render_template('welcome.html')
+  # return render_template('index.html', user_first_name=User.query.first().first_name)
 
 
 @app.route('/<name>')
@@ -62,6 +197,6 @@ def activities():
   return render_template('activities.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(ssl_context="adhoc")
 
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+from flask_security import UserMixin, RoleMixin
+from sqlalchemy import BigInteger
 
 class Team(db.Model):
   __tablename__ = "teams"
@@ -7,15 +9,31 @@ class Team(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   team_name = db.Column(db.String(250))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
   """docstring for User"""
   __tablename__ = "users"
 
-  id = db.Column(db.Integer(), primary_key = True)
-  email = db.Column(db.String(250))
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(250), unique=True)
   first_name = db.Column(db.String(250))
   last_name = db.Column(db.String(250))
   team_id = db.Column('team_id',db.Integer, db.ForeignKey('teams.id'), nullable=True)
+  active = db.Column(db.Boolean)
+  roles = db.relationship('Role', secondary='roles_users', backref=db.backref('users', lazy='dynamic'))
+
+class Role(db.Model, RoleMixin):
+  __tablename__ = 'roles'
+
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(50), unique = True)
+  description = db.Column(db.String(255))
+
+class RolesUsers(db.Model):
+  __tablename__ = 'roles_users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+  role_id = db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
 
 class Activity(db.Model):
   __tablename__ = "activities"
